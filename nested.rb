baf415b28903211a56da8cde84f422ec0a1107de
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-04 19:02:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["Assembly"]
+		 programmer_hash
 end
 def changing_alan
 	programmer_hash =
