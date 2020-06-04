6f9a02b0aa666e053d2da060381b0b9e8c5e7d49
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-04 18:58:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,31 +81,9 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {}
-}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 programmer_hash
-
-end
-binding.pry
-def changing_alan
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-
-
 end
 
 def adding_to_dennis
