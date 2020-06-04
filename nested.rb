41b361ad471266a3cb800e583b54abfe0eafacff
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-04 17:39:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for]=> "Unix"
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Unix"
 
 end
 
